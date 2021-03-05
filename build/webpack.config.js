37884b1061a95870780d7d9878a8d78f1457bb74
penggang <918800642@qq.com>
2021-03-05 18:39:14 +0800

    初始化

---
+++ build/webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path')
+module.exports = {
+    context: path.resolve(__dirname, '../'),
+    entry: {
+       app: './src/main.js' 
+    },
+    output: {
+        
+    }
+}
---
+++ src/main.js
@@ -0,0 +1,4 @@
+console.log(1232)
+import Vue from 'vue'
+
+Vue.config.productionTip = false;
